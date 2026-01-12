7f2423a426cc402613b93b999e6ff722e2ab260c
Alessandra Guerinoni <ale.guerinoni@gmail.com>
2026-01-11 18:39:01 -0800

    Finalizing direction of motor

--- ball-and-wheel-project/ball-and-wheel-project.ino
+++ ball-and-wheel-project/ball-and-wheel-project.ino
@@ -5,15 +5,28 @@ static uint8_t prevA = 0;
 static uint8_t currentA = 0;
 static uint8_t currentB = 0;
 
+static uint8_t prevDir = 2;
+static uint8_t direction = 2; // 0 -> Clockwise 1 -> CC
+volatile long count = 0;
+// long lastPrintCount = 0;
+
 void setup() {
   Serial.begin(115200);
-  pinMode(encoderA, INPUT);
-  pinMode(encoderB, INPUT);
+  pinMode(encoderA, INPUT_PULLUP);
+  pinMode(encoderB, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(encoderA), checkState, CHANGE);
-  // attachInterrupt(digitalPinToInterrupt(encoderB), checkState, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoderB), checkState, CHANGE);
 }
 
 void loop() {
+  if (prevDir != direction){
+    prevDir = direction;
+    if (direction == 0){
+      Serial.println("Clockwise");
+    }else{
+      Serial.println("Counter clockwise");
+    }
+  }
 
 }
 
@@ -23,16 +36,17 @@ void checkState() {
   if (currentA == 0){
     if (prevA == 0){
       if (currentB == 1){
-        Serial.println("Clockwise");
+        direction = 0;
+        
       } else{
-        Serial.println("Counter clockwise");
+        direction = 1;
       }
     }
     if (prevA == 1){
       if (currentB == 0){
-        Serial.println("Clockwise");
+        direction=0;
       } else{
-        Serial.println("Counter clockwise");
+        direction=1;
       }
     }
   }
@@ -40,19 +54,20 @@ void checkState() {
   if(currentA == 1) {
     if (prevA == 1){
       if (currentB == 0){
-        Serial.println("Clockwise");
+        direction=0;
       }else{
-        Serial.println("Counter clockwise");
+        direction=1;
       }
     }
     if (prevA == 0){
       if (currentB == 1){
-        Serial.println("Clockwise");
+        direction=0;
       }else{
-        Serial.println("Counter clockwise");
+        direction=1;
       }
     }
   }
 
   prevA = currentA;
+  count++;
 }
