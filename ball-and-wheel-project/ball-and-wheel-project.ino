6ebc864707b92d6749787b6c013d6c9a099300bc
Alessandra Guerinoni <ale.guerinoni@gmail.com>
2026-01-11 18:06:51 -0800

    Determining direction of motor

--- ball-and-wheel-project/ball-and-wheel-project.ino
+++ ball-and-wheel-project/ball-and-wheel-project.ino
@@ -1,10 +1,58 @@
-void setup() {
-  // put your setup code here, to run once:
-  // hi
+const byte encoderA = 2;  // input pin that the interruption will be attached to
+const byte encoderB = 7;  // input pin that the interruption will be attached to
 
+static uint8_t prevA = 0;
+static uint8_t currentA = 0;
+static uint8_t currentB = 0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(encoderA, INPUT);
+  pinMode(encoderB, INPUT);
+  attachInterrupt(digitalPinToInterrupt(encoderA), checkState, CHANGE);
+  // attachInterrupt(digitalPinToInterrupt(encoderB), checkState, CHANGE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+}
+
+void checkState() {
+  currentA = digitalRead(encoderA);
+  currentB = digitalRead(encoderB);
+  if (currentA == 0){
+    if (prevA == 0){
+      if (currentB == 1){
+        Serial.println("Clockwise");
+      } else{
+        Serial.println("Counter clockwise");
+      }
+    }
+    if (prevA == 1){
+      if (currentB == 0){
+        Serial.println("Clockwise");
+      } else{
+        Serial.println("Counter clockwise");
+      }
+    }
+  }
+  
+  if(currentA == 1) {
+    if (prevA == 1){
+      if (currentB == 0){
+        Serial.println("Clockwise");
+      }else{
+        Serial.println("Counter clockwise");
+      }
+    }
+    if (prevA == 0){
+      if (currentB == 1){
+        Serial.println("Clockwise");
+      }else{
+        Serial.println("Counter clockwise");
+      }
+    }
+  }
+
+  prevA = currentA;
 }
